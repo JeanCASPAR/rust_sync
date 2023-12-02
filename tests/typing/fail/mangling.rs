c0744a87a303c9c086872f6d24701b58913b4e21
jthulhu <adrien.lc.mathieu@gmail.com>
2023-12-02 11:56:10 +0100

    [tests] Autosabotation: j'ai trouvé un bug...

---
+++ tests/typing/fail/mangling.rs
@@ -0,0 +1,19 @@
+use rust_sync::sync;
+
+sync! {
+    node oui() = (a)
+    where
+        c : bool = false,
+        a : int on c = 3 when c;
+
+    node non() = ()
+    where
+        c : bool = true,
+        a : int on c = oui(),
+        l : int = merge c {
+            true => a,
+            false => 1 whennot c,
+        };
+}
+
+fn main() {}
