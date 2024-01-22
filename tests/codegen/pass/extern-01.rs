f409a3c8fd4cdb7792121f6ee5cf9a18f71b3cc8
jthulhu <adrien.lc.mathieu@gmail.com>
2024-01-22 09:32:10 +0100

    [tests/codegen] Added extern Rust function test.

---
+++ tests/codegen/pass/extern-01.rs
@@ -0,0 +1,31 @@
+use rustre::sync;
+
+fn f(x: f64) -> f64 {
+    x * 2.
+}
+
+sync! {
+    #![pass(3)]
+
+    node non(c : bool) = (a,)
+    where
+        a : float = merge c {
+            true => 3.0 when c,
+            false => 2.0 whennot c,
+        };
+
+    #[export]
+    node oui(c : bool) = (d)
+    where
+        b : float = 0.0 -> pre spawn non(c),
+        d : float = f(b);
+}
+
+fn main() {
+    assert_eq!(
+        sync::oui([true, false, true, false, true].into_iter().map(|x| (x,)))
+            .map(|(d,)| d)
+            .collect::<Vec<_>>(),
+        [0.0, 6.0, 4.0, 6.0, 4.0],
+    )
+}
